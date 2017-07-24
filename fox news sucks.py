ee4fb6b4c4914ab8f60fce74fcb5999267a367d9
bigbluecow <chami.ruwa@visal168.gq>
2017-07-24 11:10:38 -0400

    Add files via upload

---
+++ fox news sucks.py
@@ -0,0 +1,22 @@
+"""numbers=[1, 2, 3, 4, 5]
+
+numbers1 = []
+
+for num in numbers:
+    square = num**2
+    numbers1.append(square)
+print (numbers1)"""
+
+"""nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for item in nested_list:
+    for createdvariable in item:
+        print(createdvariable)"""
+
+prices = {"Banana": 4, "Apple":  2, "Orange": 1.5, "Pear": 3}
+stocks = {"Banana": 6, "Apple": 0, "Orange": 32, "Pear": 15}
+
+for hi in prices:
+    print (hi)
+for bye in stocks:
+    print(bye)
+    
